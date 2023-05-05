b18a845bd4aecbeb07c438440dbce600ae24d311
huhangbo.hhb <huhangbo.hhb@bytedance.com>
2023-05-05 14:02:41 +0800

    json

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"financial_management/setting"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
+	"os"
 )
 
 var (
@@ -11,9 +14,15 @@ var (
 
 func main() {
 	setting.LoadConfig("./setting/config.json")
+
+	f, _ := os.Create(setting.Config.LogConfig.Path)
+	gin.DefaultWriter = io.MultiWriter(f)
+
 	gin.SetMode(setting.Config.Mode)
+
 	h = gin.Default()
 
 	initRouter()
-	panic(h.Run())
+
+	panic(h.Run(fmt.Sprintf(":%d", setting.Config.Port)))
 }
